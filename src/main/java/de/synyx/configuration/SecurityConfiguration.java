741e407cc235aaaa9981dcdcd48e328024c2b68f
Yannic Klem <klem@synyx.de>
2015-02-18 17:59:27 +0100

    Authentifizierung gegen synyx Ldap

--- src/main/java/de/synyx/configuration/SecurityConfiguration.java
+++ src/main/java/de/synyx/configuration/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package de.synyx.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+
 
 /**
  * Created by klem on 18.02.15.
@@ -15,21 +17,17 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value(value = "${ldap.hostUrl}")
+    private String ldapHostUrl;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
                 .fullyAuthenticated().and().formLogin();
     }
 
-    @Configuration
-    protected static class AuthenticationConfiguration extends
-            GlobalAuthenticationConfigurerAdapter {
-
-        @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception {
-            auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
-                    .groupSearchBase("ou=groups").contextSource()
-                    .ldif("classpath:test-server.ldif");
-        }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=people").contextSource().url(ldapHostUrl);
     }
 }
