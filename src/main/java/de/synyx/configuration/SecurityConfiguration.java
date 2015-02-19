40de9cf4a94320e47c4a1ab2cb40e80ce2f3f0d1
Yannic Klem <klem@synyx.de>
2015-02-19 15:33:23 +0100

    Login als Formlogin (probleme mit httpBasic)

--- src/main/java/de/synyx/configuration/SecurityConfiguration.java
+++ src/main/java/de/synyx/configuration/SecurityConfiguration.java
@@ -1,12 +1,18 @@
 package de.synyx.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 /**
@@ -20,10 +26,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value(value = "${ldap.hostUrl}")
     private String ldapHostUrl;
 
+    @Autowired
+    BasicLogoutSuccessHandler basicLogoutSuccessHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
-                .fullyAuthenticated().and().formLogin();
+        http
+                .authorizeRequests().anyRequest().authenticated().
+                and().formLogin().permitAll().
+                and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll().
+                logoutSuccessHandler(basicLogoutSuccessHandler);
     }
 
     @Override
