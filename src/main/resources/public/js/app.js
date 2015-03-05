d600ae0d73b55714dc6ba97e8f83f91f63504319
Yannic Klem <klem@synyx.de>
2015-03-05 11:16:20 +0100

    Laden der Komponenten nach Angular Bootstrap ermöglicht

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,14 +1,65 @@
 var selfservice = angular.module('Selfservice', ['ngRoute', 'ngTouch', 'ui.bootstrap']);
 
-selfservice.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/'});
-}]);
+selfservice.config(['$routeProvider', '$sceDelegateProvider', '$controllerProvider', '$provide', '$compileProvider',
+    function($routeProvider,$sceDelegateProvider, $controllerProvider, $provide, $compileProvider) {
+        $routeProvider.otherwise({redirectTo: '/'});
+        $sceDelegateProvider.resourceUrlWhitelist([
+            // Allow same origin resource loads.
+            'self',
+            // Allow loading from our assets domain.  Notice the difference between * and **.
+            'http://localhost:8090/**'
+        ]);
 
-selfservice.config(function($sceDelegateProvider) {
-    $sceDelegateProvider.resourceUrlWhitelist([
-        // Allow same origin resource loads.
-        'self',
-        // Allow loading from our assets domain.  Notice the difference between * and **.
-        'http://localhost:8090/**'
-    ])
-});
+        selfservice._controller = selfservice.controller;
+        selfservice._service = selfservice.service;
+        selfservice._factory = selfservice.factory;
+        selfservice._value = selfservice.value;
+        selfservice._directive = selfservice.directive;
+        selfservice.addRoute = function(callback){
+            callback($routeProvider);
+        };
+
+        // Provider-based controller.
+        selfservice.controller = function( name, constructor ) {
+
+            $controllerProvider.register( name, constructor );
+            return( this );
+
+        };
+
+        // Provider-based service.
+        selfservice.service = function( name, constructor ) {
+
+            $provide.service( name, constructor );
+            return( this );
+
+        };
+
+        // Provider-based factory.
+        selfservice.factory = function( name, factory ) {
+
+            $provide.factory( name, factory );
+            return( this );
+
+        };
+
+        // Provider-based value.
+        selfservice.value = function( name, value ) {
+
+            $provide.value( name, value );
+            return( this );
+
+        };
+
+        // Provider-based directive.
+        selfservice.directive = function( name, factory ) {
+
+            $compileProvider.directive( name, factory );
+            return( this );
+
+        };
+    }
+]);
+
+
+
