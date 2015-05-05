902d43babe60de9126f1a87297a3306bc27b9416
Yannic Klem <klem@synyx.de>
2015-05-05 10:11:36 +0200

    Authserver mit JDBC Tokenstore

--- auth/src/main/java/de/synyx/selfservice/auth/security/OAuth2AuthorizationServerConfig.java
+++ auth/src/main/java/de/synyx/selfservice/auth/security/OAuth2AuthorizationServerConfig.java
@@ -1,16 +1,19 @@
 package de.synyx.selfservice.auth.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
-/**
- * Created by klem on 13.04.15.
- */
+import javax.sql.DataSource;
+
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -18,9 +21,17 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private TokenStore tokenStore;
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception{//NOSONAR
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
     }
 
     @Override
@@ -30,5 +41,17 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
                 .secret("selfservicesecret")
                 .authorizedGrantTypes("authorization_code", "refresh_token",
                         "password").scopes("openid");
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public DefaultTokenServices defaultTokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
     }
 }
