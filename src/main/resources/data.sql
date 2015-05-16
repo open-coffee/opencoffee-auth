cc604e43a41ad3c8b2d4e0c54378853126464c65
Yannic Klem <klem@synyx.de>
2015-05-16 15:02:10 +0200

    ClientDetails von selfservice in data.sql gelagert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO oauth_client_details VALUES (
+  'selfservice',
+  null,
+  'selfservicesecret',
+  'openid',
+  'authorization_code,password,refresh_token',
+  null,
+  null,
+  null,
+  NULL,
+  null,
+  ''
+);
