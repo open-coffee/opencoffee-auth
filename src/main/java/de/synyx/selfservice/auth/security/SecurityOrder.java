d31295751257b74e680d90853dc2b59e6a5ce4bb
Yannic Klem <klem@synyx.de>
2015-05-16 13:00:12 +0200

    Klassen ohne public konstruktor sollten final sein

--- src/main/java/de/synyx/selfservice/auth/security/SecurityOrder.java
+++ src/main/java/de/synyx/selfservice/auth/security/SecurityOrder.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 /**
  * Created by klem on 16.05.15.
  */
-public class SecurityOrder {
+public final class SecurityOrder {
 
     public static final int OVERRIDE_DEFAULT_ORDER = SecurityProperties.DEFAULT_FILTER_ORDER - 1;
 
