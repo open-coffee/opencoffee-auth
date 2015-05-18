75bd5efe2aa2cc18a3fcd85bdc6fb11cdbd47371
Yannic Klem <klem@synyx.de>
2015-05-18 10:56:07 +0200

    keine gute idee

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,49 +1,49 @@
 create table oauth_client_details (
-  client_id VARCHAR(255) PRIMARY KEY,
-  resource_ids VARCHAR(255),
-  client_secret VARCHAR(255),
-  scope VARCHAR(255),
-  authorized_grant_types VARCHAR(255),
-  web_server_redirect_uri VARCHAR(255),
-  authorities VARCHAR(255),
+  client_id VARCHAR(256) PRIMARY KEY,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additional_information VARCHAR(4096),
-  autoapprove VARCHAR(255)
+  autoapprove VARCHAR(256)
 );
 
 create table oauth_client_token (
-  token_id VARCHAR(255),
+  token_id VARCHAR(256),
   token LONGVARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255)
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256)
 );
 
 create table oauth_access_token (
-  token_id VARCHAR(255),
+  token_id VARCHAR(256),
   token LONGVARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255),
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
   authentication LONGVARBINARY,
-  refresh_token VARCHAR(255)
+  refresh_token VARCHAR(256)
 );
 
 create table oauth_refresh_token (
-  token_id VARCHAR(255),
+  token_id VARCHAR(256),
   token LONGVARBINARY,
   authentication LONGVARBINARY
 );
 
 create table oauth_code (
-  code VARCHAR(255), authentication LONGVARBINARY
+  code VARCHAR(256), authentication LONGVARBINARY
 );
 
 create table oauth_approvals (
-	userId VARCHAR(255),
-	clientId VARCHAR(255),
-	scope VARCHAR(255),
+	userId VARCHAR(256),
+	clientId VARCHAR(256),
+	scope VARCHAR(256),
 	status VARCHAR(10),
 	expiresAt TIMESTAMP,
 	lastModifiedAt TIMESTAMP
