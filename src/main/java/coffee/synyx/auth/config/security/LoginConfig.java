8b0fc100b61bf31da64ffba5d42f8512c2229445
Yannic Klem <klem@synyx.de>
2016-07-15 17:14:54 +0200

    Tested and fixed LogoutController

--- src/main/java/coffee/synyx/auth/config/security/LoginConfig.java
+++ src/main/java/coffee/synyx/auth/config/security/LoginConfig.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.core.annotation.Order;
 
+import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -53,9 +55,12 @@ public class LoginConfig extends WebSecurityConfigurerAdapter {
             .and()
             .logout()
             .logoutUrl("/logout")
-            .permitAll()
             .logoutSuccessHandler(logoutSuccessHandler)
             .deleteCookies(serverProperties.getSession().getCookie().getName())
+            .and()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/logout")
+            .permitAll()
             .and()
             .authorizeRequests()
             .anyRequest()
