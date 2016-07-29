48b19c99dbcac00b6da6694916542b5fbe8eb6e5
Yannic Klem <yann.klem@gmail.com>
2016-07-29 11:32:17 +0200

    Core concept of the crud functionallity for ClientDetails in Java created.

--- src/main/java/coffee/synyx/auth/oauth/web/AuthClientController.java
+++ src/main/java/coffee/synyx/auth/oauth/web/AuthClientController.java
@@ -1,38 +1,64 @@
 package coffee.synyx.auth.oauth.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 
 /**
  * @author  Tobias Schneider - schneider@synyx.de
+ * @author  Yannic Klem - klem@synyx.de
  */
 @Controller
-@RequestMapping("/authclient")
+@RequestMapping("/clients")
 public class AuthClientController {
 
-    private AuthClientService authClientService;
+    private JdbcClientDetailsService jdbcClientDetailsService;
 
     @Autowired
-    public AuthClientController(AuthClientService authClientService) {
+    public AuthClientController(JdbcClientDetailsService jdbcClientDetailsService) {
 
-        this.authClientService = authClientService;
+        this.jdbcClientDetailsService = jdbcClientDetailsService;
+    }
+
+    @RequestMapping(method = GET)
+    public String getAllClientsView(Model model) {
+
+        List<ClientDetailsResource> clientDetails = jdbcClientDetailsService.listClientDetails()
+                .stream()
+                .map(ClientDetailsResource::new)
+                .collect(Collectors.toList());
+
+        model.addAttribute("clients", clientDetails);
+
+        return "clients/all";
     }
 
     @RequestMapping(value = "/{authClientId}", method = GET)
     public String getById(@PathVariable("authClientId") String authClientId, Model model) {
 
-        AuthClient authClient = authClientService.get(authClientId);
-        model.addAttribute("authClient", authClient);
 
-        return "authClient";
+        ClientDetails clientDetails = jdbcClientDetailsService.loadClientByClientId(authClientId);
+        model.addAttribute("client", new ClientDetailsResource(clientDetails));
+
+        return "clients/specific";
+    }
+
+    @RequestMapping(value = "/{authClientId}", method = DELETE)
+    public String deleteClient(@PathVariable("authClientId") String authClientId) {
+
+        jdbcClientDetailsService.removeClientDetails(authClientId);
+
+        return "clients/all";
     }
 }
---
+++ src/main/java/coffee/synyx/auth/oauth/web/ClientDetailsResource.java
@@ -0,0 +1,185 @@
+package coffee.synyx.auth.oauth.web;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Dto of the {@link ClientDetails}
+ *
+ * @author  Yannic Klem - klem@synyx.de
+ */
+public class ClientDetailsResource {
+
+    private String clientId;
+    private String clientSecret;
+    private String resourceIds;
+    private String scope;
+    private String authorizedGrantTypes;
+    private String registeredRedirectUri;
+    private String authorities;
+    private Integer accessTokenValidity;
+    private Integer refreshTokenValidity;
+    private boolean autoApprove;
+    private Map<String, Object> additionalInformation;
+
+    public ClientDetailsResource(ClientDetails clientDetails){
+
+        this.clientId = clientDetails.getClientId();
+        this.clientSecret = clientDetails.getClientSecret();
+        this.resourceIds = StringUtils.collectionToCommaDelimitedString(clientDetails.getResourceIds());
+        this.scope = StringUtils.collectionToCommaDelimitedString(clientDetails.getScope());
+        this.authorizedGrantTypes = StringUtils.collectionToCommaDelimitedString(clientDetails.getAuthorizedGrantTypes());
+        this.registeredRedirectUri = StringUtils.collectionToCommaDelimitedString(clientDetails.getRegisteredRedirectUri());
+        this.authorities = StringUtils.collectionToCommaDelimitedString(clientDetails.getAuthorities());
+        this.accessTokenValidity = clientDetails.getAccessTokenValiditySeconds();
+        this.refreshTokenValidity = clientDetails.getRefreshTokenValiditySeconds();
+        this.additionalInformation = clientDetails.getAdditionalInformation();
+    }
+
+    public ClientDetails toEntity() {
+
+        AuthClient authClient = new AuthClient();
+        authClient.setClientId(clientId);
+        authClient.setClientSecret(clientSecret);
+        authClient.setResourceIds(StringUtils.commaDelimitedListToSet(resourceIds));
+        authClient.setScope(StringUtils.commaDelimitedListToSet(scope));
+        authClient.setAuthorizedGrantTypes(StringUtils.commaDelimitedListToSet(authorizedGrantTypes));
+        authClient.setRegisteredRedirectUri(StringUtils.commaDelimitedListToSet(registeredRedirectUri));
+
+        List<GrantedAuthority> authorities = StringUtils.commaDelimitedListToSet(this.authorities)
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        authClient.setAuthorities(authorities);
+
+        authClient.setAccessTokenValiditySeconds(accessTokenValidity);
+        authClient.setRefreshTokenValiditySeconds(refreshTokenValidity);
+        authClient.setAutoApprove(autoApprove);
+        authClient.setAdditionalInformation(additionalInformation);
+
+        return authClient;
+    }
+
+    public String getClientId() {
+
+        return clientId;
+    }
+
+
+    public void setClientId(String clientId) {
+
+        this.clientId = clientId;
+    }
+
+
+    public String getClientSecret() {
+
+        return clientSecret;
+    }
+
+
+    public void setClientSecret(String clientSecret) {
+
+        this.clientSecret = clientSecret;
+    }
+
+
+    public String getResourceIds() {
+
+        return resourceIds;
+    }
+
+
+    public void setResourceIds(String resourceIds) {
+
+        this.resourceIds = resourceIds;
+    }
+
+
+    public String getScope() {
+
+        return scope;
+    }
+
+
+    public void setScope(String scope) {
+
+        this.scope = scope;
+    }
+
+
+    public String getAuthorizedGrantTypes() {
+
+        return authorizedGrantTypes;
+    }
+
+
+    public void setAuthorizedGrantTypes(String authorizedGrantTypes) {
+
+        this.authorizedGrantTypes = authorizedGrantTypes;
+    }
+
+
+    public String getRegisteredRedirectUri() {
+
+        return registeredRedirectUri;
+    }
+
+
+    public void setRegisteredRedirectUri(String registeredRedirectUri) {
+
+        this.registeredRedirectUri = registeredRedirectUri;
+    }
+
+
+    public String getAuthorities() {
+
+        return authorities;
+    }
+
+
+    public void setAuthorities(String authorities) {
+
+        this.authorities = authorities;
+    }
+
+
+    public Integer getAccessTokenValidity() {
+        return accessTokenValidity;
+    }
+
+    public void setAccessTokenValidity(Integer accessTokenValidity) {
+        this.accessTokenValidity = accessTokenValidity;
+    }
+
+    public Integer getRefreshTokenValidity() {
+        return refreshTokenValidity;
+    }
+
+    public void setRefreshTokenValidity(Integer refreshTokenValidity) {
+        this.refreshTokenValidity = refreshTokenValidity;
+    }
+
+    public boolean isAutoApprove() {
+        return autoApprove;
+    }
+
+    public void setAutoApprove(boolean autoApprove) {
+        this.autoApprove = autoApprove;
+    }
+
+    public Map<String, Object> getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(Map<String, Object> additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+}
