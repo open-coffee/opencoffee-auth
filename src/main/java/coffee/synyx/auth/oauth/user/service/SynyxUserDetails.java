098777ef4a8300b18ed2bd5449765ffb15fab85b
Tobias Schneider <schneider@synyx.de>
2016-07-31 21:32:09 +0200

    converts and do not keep the ldap user in the specific synyx object

--- src/main/java/coffee/synyx/auth/oauth/user/service/SynyxUserDetails.java
+++ src/main/java/coffee/synyx/auth/oauth/user/service/SynyxUserDetails.java
@@ -4,24 +4,38 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static org.springframework.security.core.authority.AuthorityUtils.authorityListToSet;
 
-public class SynyxUserDetails implements LdapUserDetails {
 
-    private static final long serialVersionUID = 1L;
+public final class SynyxUserDetails implements LdapUserDetails {
 
-    private String mail;
-    private LdapUserDetails details;
+    private final String mail;
+    private final String dn;
+    private final boolean enabled;
+    private final Collection<? extends GrantedAuthority> authorities;
+    private final String username;
+    private final String password;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
 
-    public SynyxUserDetails(LdapUserDetails details) {
+    public SynyxUserDetails(LdapUserDetails details, String mail) {
 
-        this.details = details;
+        this.mail = mail;
+        this.dn = details.getDn();
+        this.enabled = details.isEnabled();
+        this.authorities = details.getAuthorities();
+        this.username = details.getUsername();
+        this.password = details.getPassword();
+        this.accountNonExpired = details.isAccountNonExpired();
+        this.accountNonLocked = details.isAccountNonLocked();
+        this.credentialsNonExpired = details.isCredentialsNonExpired();
     }
 
     public String getMail() {
@@ -30,16 +44,10 @@ public class SynyxUserDetails implements LdapUserDetails {
     }
 
 
-    public void setMail(String mail) {
-
-        this.mail = mail;
-    }
-
-
     @Override
     public boolean isEnabled() {
 
-        return details.isEnabled();
+        return enabled;
     }
 
 
@@ -47,7 +55,7 @@ public class SynyxUserDetails implements LdapUserDetails {
     @JsonIgnore
     public String getDn() {
 
-        return details.getDn();
+        return dn;
     }
 
 
@@ -55,14 +63,14 @@ public class SynyxUserDetails implements LdapUserDetails {
     @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
 
-        return details.getAuthorities();
+        return authorities;
     }
 
 
     @JsonProperty("authorities")
     private List<String> getAuthoritiesAsStrings() {
 
-        return new ArrayList<>(AuthorityUtils.authorityListToSet(details.getAuthorities()));
+        return new ArrayList<>(authorityListToSet(authorities));
     }
 
 
@@ -70,34 +78,34 @@ public class SynyxUserDetails implements LdapUserDetails {
     @JsonIgnore
     public String getPassword() {
 
-        return details.getPassword();
+        return password;
     }
 
 
     @Override
     public String getUsername() {
 
-        return details.getUsername();
+        return username;
     }
 
 
     @Override
     public boolean isAccountNonExpired() {
 
-        return details.isAccountNonExpired();
+        return accountNonExpired;
     }
 
 
     @Override
     public boolean isAccountNonLocked() {
 
-        return details.isAccountNonLocked();
+        return accountNonLocked;
     }
 
 
     @Override
     public boolean isCredentialsNonExpired() {
 
-        return details.isCredentialsNonExpired();
+        return credentialsNonExpired;
     }
 }
