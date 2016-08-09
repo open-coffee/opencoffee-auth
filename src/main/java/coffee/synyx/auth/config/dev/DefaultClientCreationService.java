8c9ce0b521caa32c3733a6902a7db3cb4b862fa1
Yannic Klem <klem@synyx.de>
2016-08-09 09:37:35 +0200

    separate Configuration and Service

--- src/main/java/coffee/synyx/auth/config/dev/DefaultClientCreationService.java
+++ src/main/java/coffee/synyx/auth/config/dev/DefaultClientCreationService.java
@@ -1,19 +1,12 @@
-package coffee.synyx.auth.config;
+package coffee.synyx.auth.config.dev;
 
 import coffee.synyx.auth.oauth.web.AuthClient;
 
 import org.slf4j.Logger;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import org.springframework.context.annotation.DependsOn;
 
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
@@ -25,35 +18,16 @@ import static java.lang.invoke.MethodHandles.lookup;
 /**
  * @author  Yannic Klem - klem@synyx.de
  */
-@Component
 @DependsOn("liquibase")
-@ConfigurationProperties(prefix = "coffeenet")
-@ConditionalOnProperty(prefix = "coffeenet", name = "development", havingValue = "true", matchIfMissing = true)
-public class DevelopmentConfiguration {
+public class DefaultClientCreationService {
 
     private static final Logger LOGGER = getLogger(lookup().lookupClass());
-
     private final JdbcClientDetailsService jdbcClientDetailsService;
 
-    private boolean development;
-
-    @Autowired
-    public DevelopmentConfiguration(JdbcClientDetailsService jdbcClientDetailsService) {
+    public DefaultClientCreationService(JdbcClientDetailsService jdbcClientDetailsService) {
 
         this.jdbcClientDetailsService = jdbcClientDetailsService;
     }
-
-    public boolean isDevelopment() {
-
-        return development;
-    }
-
-
-    public void setDevelopment(boolean development) {
-
-        this.development = development;
-    }
-
 
     @PostConstruct
     public void createDefaultClient() {
