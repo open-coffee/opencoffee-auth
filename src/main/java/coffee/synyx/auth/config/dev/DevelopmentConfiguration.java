f139c7e914093956dfc8939f79ef591af65ee7f3
Tobias Schneider <schneider@synyx.de>
2016-08-12 10:19:33 +0200

    Move DefaultClientCreationService logic directly into DevelopmentConfiguration
    * avoid exposing beans that are not needed for the users
    * encapsulation

--- src/main/java/coffee/synyx/auth/config/dev/DevelopmentConfiguration.java
+++ src/main/java/coffee/synyx/auth/config/dev/DevelopmentConfiguration.java
@@ -1,13 +1,22 @@
 package coffee.synyx.auth.config.dev;
 
+import coffee.synyx.auth.oauth.web.AuthClient;
+
+import org.slf4j.Logger;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+
+import javax.annotation.PostConstruct;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import static java.lang.invoke.MethodHandles.lookup;
 
 
 /**
@@ -18,6 +27,15 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 @ConfigurationProperties(prefix = "coffeenet")
 @ConditionalOnProperty(prefix = "coffeenet", name = "development", havingValue = "true", matchIfMissing = true)
 public class DevelopmentConfiguration {
+
+    private static final Logger LOGGER = getLogger(lookup().lookupClass());
+
+    private final JdbcClientDetailsService jdbcClientDetailsService;
+
+    public DevelopmentConfiguration(JdbcClientDetailsService jdbcClientDetailsService) {
+
+        this.jdbcClientDetailsService = jdbcClientDetailsService;
+    }
 
     private boolean development;
 
@@ -33,9 +51,16 @@ public class DevelopmentConfiguration {
     }
 
 
-    @Bean
-    public DefaultClientCreationService defaultClientCreationService(JdbcClientDetailsService clientDetailsService) {
+    @PostConstruct
+    void createDefaultClient() {
 
-        return new DefaultClientCreationService(clientDetailsService);
+        LOGGER.info("Adding default OAuth Client: testClient/testClientSecret");
+
+        AuthClient authClient = new AuthClient();
+        authClient.setAutoApprove(true);
+        authClient.setClientId("testClient");
+        authClient.setClientSecret("testClientSecret");
+        authClient.getScope().add("openid");
+        jdbcClientDetailsService.addClientDetails(authClient);
     }
 }
