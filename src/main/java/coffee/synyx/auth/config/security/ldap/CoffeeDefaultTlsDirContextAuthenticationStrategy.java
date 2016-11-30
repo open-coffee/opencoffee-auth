4eddab9baaf80075082aeb59c8d0b0ce55742125
Tobias Schneider <schneider@synyx.de>
2016-11-30 21:20:45 +0100

    Add a little bit of javadoc for the tls fix

--- src/main/java/coffee/synyx/auth/config/security/ldap/CoffeeDefaultTlsDirContextAuthenticationStrategy.java
+++ src/main/java/coffee/synyx/auth/config/security/ldap/CoffeeDefaultTlsDirContextAuthenticationStrategy.java
@@ -20,6 +20,8 @@ public class CoffeeDefaultTlsDirContextAuthenticationStrategy extends AbstractTl
         ctx.addToEnvironment(Context.SECURITY_AUTHENTICATION, SIMPLE_AUTHENTICATION);
         ctx.addToEnvironment(Context.SECURITY_PRINCIPAL, userDn);
         ctx.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
+
+        // Force reconnect with user credentials
         ctx.reconnect(null);
     }
 }
