40722ae51ca2db33781711ddcd0cc9bb0c5ff7c5
Tobias Schneider <tobias.schneider@tobsch.org>
2016-12-04 18:11:14 +0100

    Renaming Packages/Classes
    * AuthServerConfigurationProperties -> AuthConfigurationProperties
    * coffee.synyx.auth.config.security -> coffee.synyx.auth.config.integration.security
    * LdapConfigurationProperties -> AuthLdapConfigurationProperties

--- src/main/java/coffee/synyx/auth/web/LogoutController.java
+++ src/main/java/coffee/synyx/auth/web/LogoutController.java
@@ -1,6 +1,6 @@
 package coffee.synyx.auth.web;
 
-import coffee.synyx.auth.config.AuthServerConfigurationProperties;
+import coffee.synyx.auth.config.AuthConfigurationProperties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,15 +20,15 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * @author  Yannic Klem - klem@synyx.de
  */
 @Controller
-@EnableConfigurationProperties(AuthServerConfigurationProperties.class)
+@EnableConfigurationProperties(AuthConfigurationProperties.class)
 public class LogoutController {
 
-    private final AuthServerConfigurationProperties authServerConfigurationProperties;
+    private final AuthConfigurationProperties authConfigurationProperties;
 
     @Autowired
-    public LogoutController(AuthServerConfigurationProperties authServerConfigurationProperties) {
+    public LogoutController(AuthConfigurationProperties authConfigurationProperties) {
 
-        this.authServerConfigurationProperties = authServerConfigurationProperties;
+        this.authConfigurationProperties = authConfigurationProperties;
     }
 
     /**
@@ -47,7 +47,7 @@ public class LogoutController {
 
         if (principal == null) {
             if (referrer == null) {
-                view = "redirect:" + authServerConfigurationProperties.getDefaultRedirectUrl();
+                view = "redirect:" + authConfigurationProperties.getDefaultRedirectUrl();
             } else {
                 view = "redirect:" + referrer;
             }
