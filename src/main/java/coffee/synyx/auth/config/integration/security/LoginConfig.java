f44d6b7883717e44f822c796aa739f4ae61c6195
Tobias Schneider <schneider@synyx.de>
2017-06-14 12:02:58 +0200

    remove ignoring of h2-console in LoginConfig

--- src/main/java/coffee/synyx/auth/config/integration/security/LoginConfig.java
+++ src/main/java/coffee/synyx/auth/config/integration/security/LoginConfig.java
@@ -18,7 +18,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -60,15 +59,6 @@ public class LoginConfig extends WebSecurityConfigurerAdapter {
 
         LOGGER.info("//> LoginConfig...");
     }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-
-        web.ignoring().antMatchers("/h2-console/**");
-
-        LOGGER.info("//> LoginConfig does ignore h2-console/**");
-    }
-
 
     @Override
     public void configure(HttpSecurity http) throws Exception { // NOSONAR
