229ae61babe688ff8dbbf1b9881f581aa2671fa9
Tobias Schneider <schneider@synyx.de>
2017-06-16 15:42:38 +0200

    add @Validated to AuthorizationProperties

--- src/main/java/coffee/synyx/auth/authorization/config/AuthorizationProperties.java
+++ src/main/java/coffee/synyx/auth/authorization/config/AuthorizationProperties.java
@@ -4,10 +4,13 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import org.springframework.validation.annotation.Validated;
+
 
 /**
  * @author  Tobias Schneider
  */
+@Validated
 @ConfigurationProperties(prefix = "auth.key")
 public class AuthorizationProperties {
 
